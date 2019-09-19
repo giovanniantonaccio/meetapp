67577b85499bc201928bfd004bd0bce432d20f6d
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-09-19 13:12:23 -0400

    add router basic config

---
+++ web/src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import NotFound from '../pages/NotFound';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
