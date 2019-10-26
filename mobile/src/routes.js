ebebc2e646fce796971ae8859b8f3196e43565c3
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-10-26 09:23:11 -0400

    first app commit
    - Configure eslint, prettier and editorconfig
    - Install all libs
    - Configure Redux
    - Configure Reactotron
    - Link React Native Icons
    - Implement SignIn page
    - Implement SignUp page
    - Adjust Router
    - Redirect user based on authentication

---
+++ mobile/src/routes.js
@@ -0,0 +1,45 @@
+// import React from 'react';
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+  // createStackNavigator,
+} from 'react-navigation';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import Meetups from './pages/Meetups';
+import Profile from './pages/Profile';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Meetups,
+            Profile,
+          },
+          {
+            resetOnBlur: true,
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255,255,255,0.5)',
+              style: {
+                backgroundColor: '#402845',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
